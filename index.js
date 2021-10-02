e49bf709aa04d93448ac58134cc7704d82057797
Ajit Singhal <73593608+AjCodez@users.noreply.github.com>
2021-10-02 18:44:27 +0530

    styled bar etc

--- index.js
+++ index.js
@@ -2,15 +2,15 @@ var color = document.getElementById("color");
 var data = document.getElementById("data-used");
 function increaseFull(){
     color.style.width = "100%";
-    data.innerHTML = "1000"; 
+    data.innerHTML = "1000 GB"; 
 }
 
 function increaseHalf(){
     color.style.width = "50%";
-    data.innerHTML = "500"; 
+    data.innerHTML = "500 GB"; 
 }
 
 function Empty(){
     color.style.width = "0%";
-    data.innerHTML = "0"; 
+    data.innerHTML = "0 GB"; 
 }
